1af7e7b95775c82eed5f9293157f0c0c2551f112
devromarc <rmrcslvdr@gmail.com>
2024-01-29 14:06:03 +0800

    done with Social network profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
+import css from './Profile.module.css';
 
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div className="profile">
-      <div className="description">
-        <img src={avatar} alt="User avatar" className="avatar" />
-        <p className="name">{username}</p>
-        <p className="tag">@{tag}</p>
-        <p className="location">{location}</p>
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
       </div>
 
-      <ul className="stats">
-        <li>
-          <span className="label">Followers</span>
+      <ul className={css.stats}>
+        <li className={css.item}>
+          <span className={css.label}>Followers</span>
           <span className="quantity">{stats.followers}</span>
         </li>
-        <li>
-          <span className="label">Views</span>
+        <li className={css.item}>
+          <span className={css.label}>Views</span>
           <span className="quantity">{stats.views}</span>
         </li>
-        <li>
-          <span className="label">Likes</span>
+        <li className={css.item}>
+          <span className={css.label}>Likes</span>
           <span className="quantity">{stats.likes}</span>
         </li>
       </ul>
