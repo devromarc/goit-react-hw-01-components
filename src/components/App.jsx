53e347534dffa10725f180cf90b4af84859da25c
devromarc <rmrcslvdr@gmail.com>
2024-06-11 00:48:44 +0800

    FriendList Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import { Profile } from './Profile/Profile';
 import user from '../data/user.json';
 import { Statistics } from './Statistics/Statistics';
 import data from '../data/data.json';
+import { FriendList } from './FiendList/FriendList';
+import friends from '../data/friends.json';
 export const App = () => {
   return (
     <>
@@ -13,6 +15,7 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </>
   );
 };
