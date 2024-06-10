3171f5fb85d05bebc0fe10294f78b85a6aa07a1a
devromarc <rmrcslvdr@gmail.com>
2024-06-10 23:06:25 +0800

    Statistics Done

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,46 @@
+import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
+export const Statistics = ({ title, stats }) => {
+  // Random Background Color
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul className={css.statList}>
+        {/* Array of objects. Can use the Map method */}
+        {stats.map(stat => (
+          <li
+            //   key
+            key={stat.id}
+            className={css.item}
+            style={{ backgroundColor: getRandomColor() }}
+          >
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
